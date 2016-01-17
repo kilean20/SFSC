a52d34240cf16450bc147b3507e39176a3ea115d
kilean20 <kilean20@gmail.com>
2016-01-17 12:45:27 -0500

    [Lin] ran 1E9

--- main_dE0.cpp
+++ main_dE0.cpp
@@ -2,12 +2,18 @@
 #include <vector>
 #include "efodo.h"
 #include "line.h"
+#include "global.h"
+#include <armadillo>
+
+
+using namespace arma;
+
 
 int main()
 {
     cout.precision(15);
     //Define the ring
-    Line FODO;    efodo(FODO);
+    LINE FODO;    efodo(FODO);
 
     size_t nTurn=1e9;
     size_t nSkip=1e6;
@@ -18,7 +24,7 @@ int main()
     mat sData(nTurn/nSkip+1,2);sData.zeros();
     mat spinData(nTurn/nSkip+1,2);spinData.zeros();
 
-    //x[dE_]=1.0e-4;
+    x[dE_]=-5.0e-4;
     for(size_t i=0;i<nTurn;i++){
         if (i%nSkip==0)
         {
@@ -31,7 +37,7 @@ int main()
             spinData(i/nSkip,0)=x[Sx_];
             spinData(i/nSkip,1)=x[Ss_];
         }
-        for(size_t j=0;j!=FODO.Ncell;j++) FODO.Cell[j]->Pass(x);
+        for(size_t j=0;j!=FODO.Ncell;j++) FODO.Cell[j].Pass(x);
     }
     xData(nTurn/nSkip,0)=x[x_];
     xData(nTurn/nSkip,1)=x[px_];
@@ -42,9 +48,9 @@ int main()
     spinData(nTurn/nSkip,0)=x[Sx_];
     spinData(nTurn/nSkip,1)=x[Ss_];
 
-    xData.save("xData_dE0",raw_ascii);
-    zData.save("zData_dE0",raw_ascii);
-    sData.save("sData_dE0",raw_ascii);
-    spinData.save("spinData_dE0",raw_ascii);
+    xData.save("xData_dE_5",raw_ascii);
+    zData.save("zData_dE_5",raw_ascii);
+    sData.save("sData_dE_5",raw_ascii);
+    spinData.save("spinData_dE_5",raw_ascii);
     return 0;
 }
